508027a77717556c40409b79cf9de4fcb7243178
darxx26 <akshit2k26@gmail.com>
2024-03-10 19:49:47 -0400

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import HowToUse from './pages/HowToUse/HowToUse';
 import Donate from './pages/Donate/Donate';
 import Generate from './pages/Use/Generate/Generate';
-import Grade from './pages/Use/Grade/Grade';
-import SignIn from './pages/SignIn/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <Router>
         <Header /> {/* Header appears on all pages */}
         <Routes>
           <Route path="/Home" element={<Home />} /> {/* Default route */}
@@ -21,7 +18,6 @@ function App() {
           {/* Add more routes as needed */}
         </Routes>
         <Footer /> {/* Footer appears on all pages */}
-      </Router>
     </div>
   );
 }
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+
 
 function Header() {
     return (
@@ -6,14 +9,22 @@ function Header() {
             <header>
                 <Link to="/Home">Home</Link>
                 <div class="spacer"></div>
+                <div className="dropdown">
+                <button className="dropbtn"><span>Use</span></button>
+                    <div className="dropdown-content">
+                        <Link to="/Generate">Generate</Link>
+                        <Link to="/Grade">Grade</Link>
+                    </div>
+                </div>
                 <div class="nav-links">
-                    <Link to="/Generate">Generate</Link>
                     <Link to="/HowToUse">How to use</Link>
                     <Link to="/Donate">Donate</Link>
                 </div>
+               
             </header>
         </div>
     )
 }
+
 
 export default Header;
