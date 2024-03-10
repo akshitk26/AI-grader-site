29e2827ad833e400b7e21297e3c454b82f202877
darxx26 <akshit2k26@gmail.com>
2024-03-10 19:11:08 -0400

    gonna undo this

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import HowToUse from './pages/HowToUse/HowToUse';
 import Donate from './pages/Donate/Donate';
-import Generate from './pages/Generate/Generate';
-
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
-import React from 'react';
+import Generate from './pages/Use/Generate/Generate';
+import Grade from './pages/Use/Grade/Grade';
+import SignIn from './pages/SignIn/SignIn';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
+    <div className="App">
+      <Router>
         <Header /> {/* Header appears on all pages */}
         <Routes>
           <Route path="/Home" element={<Home />} /> {/* Default route */}
           <Route path="/HowToUse" element={<HowToUse />} />
           <Route path="/Donate" element={<Donate />} />
           <Route path="/Generate" element={<Generate />} />
+          <Route path="/Grade" element={<Grade />} />
+          <Route path="/SignIn" element={<SignIn />} />
           {/* Add more routes as needed */}
         </Routes>
         <Footer /> {/* Footer appears on all pages */}
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -8,11 +8,18 @@ function Header() {
             <header>
                 <Link to="/Home">Home</Link>
                 <div class="spacer"></div>
+                <div className="dropdown">
+                <button className="dropbtn"><span>Use</span></button>
+                    <div className="dropdown-content">
+                        <Link to="/Generate">Generate</Link>
+                        <Link to="/Grade">Grade</Link>
+                    </div>
+                </div>
                 <div class="nav-links">
-                    <Link to="/Generate">Generate</Link>
                     <Link to="/HowToUse">How to use</Link>
                     <Link to="/Donate">Donate</Link>
                 </div>
+                
             </header>
         </div>
     )
