2e396f265a54e79801ba704da0bd44eb9789b4f1
darxx26 <akshit2k26@gmail.com>
2024-03-13 20:30:21 -0400

    Revert "working backend"
    This reverts commit 4c097721876962d557593398644ca153e0b9efcc.

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,38 @@
-from flask import Flask, request, jsonify
+
+# app.py
+from flask import Flask, request
+from flask_cors import CORS
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/openai', methods=['POST', 'OPTIONS'])
+def get_openai_output():
+    if request.method == 'OPTIONS':
+        return '', 200
+
+    print('Request received')
+    data = request.get_json()
+    prompt = data['prompt']
+
+    client = OpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+    )
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ]
+        )
+
+    return {'output': response.choices[0].message.content.strip()}
+
+if __name__ == '__main__':
+    app.run(port=5000)
